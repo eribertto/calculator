34fe012e376cfb2b76f801bc4a5715e72a4f88c6
eribertt <eribert.mendez@gmail.com>
2021-06-22 21:12:19 +0300

    add use declaration and some vars and functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// simple calculator app in Rust
+
+use std::io::{stdin, stdout, Write};
+
+// create a function
+fn read(input: &mut String) {
+	stdout().flush().expect("failed to flush");
+	stdin().read_line(input).expect("failed to read");
+}
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Rust simple calculator program!");
+	println!("------");
+
+	let mut num1 = String::new();
+	let mut num2 = String::new();
+	let mut operator = String::new();
+
+	println!("what is the first number?: ");
 }
