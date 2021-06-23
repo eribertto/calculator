a2fbe028ee2e503946e3aeb6f77815eea15e7031
Eriberto Mendz <erimendz@gmail.com>
2021-06-23 11:59:25 +0300

    introduce loop and print formatting

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,52 @@ fn read(input: &mut String) {
 fn main() {
     println!("Welcome to Rust simple calculator program!");
 	println!("------");
+	
+	// make the loop
+	
+	loop {
 
-	let mut num1 = String::new();
-	let mut num2 = String::new();
-	let mut operator = String::new();
-
-	print!("what is the first number?: ");
-	read(&mut num1);
-
-	print!("what is the second number?: ");
-	read(&mut num2);
-
-	print!("what operation would you like to do? [+-*/]: ");
-	read(&mut operator);
-
-	// change input to numbers from strings
-	let num1: f32 = num1.trim().parse().unwrap();
-	let num2: f32 = num2.trim().parse().unwrap();
-	let operator: char = operator.trim().chars().next().unwrap();
-
-	// do some initial debug
-	println!("num1: {} num2 {} operator: {}", num1, num2, operator);
+    	let mut num1 = String::new();
+    	let mut num2 = String::new();
+    	let mut operator = String::new();
+    
+    	print!("what is the first number?: ");
+    	read(&mut num1);
+    
+    	print!("what is the second number?: ");
+    	read(&mut num2);
+    
+    	print!("what operation would you like to do? [+-*/]: ");
+    	read(&mut operator);
+    
+    	// change input to numbers from strings
+    	let num1: f32 = num1.trim().parse().unwrap();
+    	let num2: f32 = num2.trim().parse().unwrap();
+    	let operator: char = operator.trim().chars().next().unwrap();
+    
+    	// do some initial debug
+    	println!("num1: {} num2 {} operator: {}", num1, num2, operator);
+    	
+    	let operators = String::from("+-*/");
+    	
+    	// conditionals with negation
+    	if !operators.contains(operator) {
+    	    println!("unknown operator!");
+    	    //return;
+    	    // use continue to loop over
+    	    continue;
+    	    // note: a good exercise to the newbie is to make a loop when the operator is unknown
+    	    // that way only the operator will change and not the numbers
+    	}
+    	// note the use of single quote inside match block since it is matching string literals
+    	let result = match operator {
+    	    '+' => num1 + num2,
+    	    '-' => num1 - num2,
+    	    '*' => num1 * num2,
+    	    '/' => num1 / num2,
+    	    _ => panic!("error in operator")
+    	};      // note the closing semicolon
+    	// show the operation output
+    	println!("the result of {} {} {} = {}", num1, operator, num2, result);
+	}
 }
